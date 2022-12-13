bfaef9779edcb105fe63933e9edbb2a8ab1831b1
Yingchi Long <i@lyc.dev>
2022-12-13 14:49:16 +0800

    test/NFC: add static linkage to testStorage

--- test/TestStorage.cpp
+++ test/TestStorage.cpp
@@ -7,7 +7,8 @@
 
 typedef Storage<uint64_t> TestStorage;
 
-void testCorrect(uint32_t numCases, int maxn, const TestStorage &storage) {
+static void testCorrect(uint32_t numCases, int maxn,
+                        const TestStorage &storage) {
   std::random_device rd;  // obtain a random number from hardware
   std::mt19937 gen(rd()); // seed the generator
   std::uniform_int_distribution<uint64_t> distr(1, maxn); // define the range
