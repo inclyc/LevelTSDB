19311194869348d0cea943f5ebcd1f89fcbee073
Yingchi Long <i@lyc.dev>
2022-12-13 14:14:22 +0800

    data: remove extra comment

--- include/Data.h
+++ include/Data.h
@@ -27,9 +27,7 @@ public:
     maxtime++;
   }
 
-  std::tuple<size_t, T>
-  forwarding(std::size_t l,
-             std::size_t r) const { // min(2^n, log2{r- l})
+  std::tuple<size_t, T> forwarding(std::size_t l, std::size_t r) const {
     size_t level = std::min(static_cast<size_t>(__builtin_ctz(l)),
                             static_cast<size_t>((log(r - l) / log(2))));
     return {1 << level, data[level][(l >> level) - 1]};
