3b9d2f1dd509bf3aa8d8fffc8e92bbb84cbb2c39
Yingchi Long <i@lyc.dev>
2022-12-21 00:35:26 +0800

    [map] remove virutal as use CRTP

--- include/Map.h
+++ include/Map.h
@@ -9,15 +9,13 @@ using std::size_t;
 
 template <class T> class Map {
 public:
-  virtual T &operator[](size_t) = 0;
+  T &operator[](size_t);
 };
 
 template <class T> class ArrayMap : public Map<T> {
   std::vector<T> data;
 
 public:
-  virtual ~ArrayMap() = default;
-
   T &operator[](size_t x) override {
     if (data.size() <= x) {
       data.resize(2 * x);
