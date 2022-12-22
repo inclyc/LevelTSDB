46e8ef425559e487f33586429714268980b0957e
alxndrt0512 <w1301511091@hotmail.com>
2022-12-22 11:58:20 +0800

    [map] add LRU cache, refactoring tests

--- include/Map.h
+++ include/Map.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#include <cstdint>
+#include <cstddef>
+#include <list>
+#include <unordered_map>
 #include <vector>
 
 namespace LevelTSDB {
@@ -9,13 +11,14 @@ using std::size_t;
 
 template <class T> class Map {
 public:
-  T &operator[](size_t);
+  void set(size_t, T);
+  T get(size_t);
 };
 
 template <class T> class ArrayMap : public Map<T> {
   std::vector<T> data;
 
-public:
+private:
   T &operator[](size_t x) {
     if (data.size() <= x) {
       data.resize(2 * x);
@@ -23,6 +26,77 @@ public:
     }
     return data[x];
   }
+
+public:
+  T get(size_t x) { return this->operator[](x); }
+  void set(size_t x, T value) { this->operator[](x) = value; }
 };
 
-} // namespace LevelTSDB
+template <class T, class B> class Lru {
+private:
+  struct Node {
+    size_t key;
+    T value;
+    Node(size_t key, T value) : key(key), value(value) {}
+  };
+
+  size_t cap;
+
+  std::list<Node> list;
+  std::unordered_map<size_t, typename std::list<Node>::iterator> mp;
+
+  static_assert(std::is_base_of<LevelTSDB::Map<T>, B>());
+  B base;
+
+public:
+  Lru(size_t cap) : cap(cap) {}
+  void set(size_t key, const T value) {
+    auto it = mp.find(key);
+    if (it == mp.end()) {
+      if (list.size() == cap) {
+        auto key = list.back().key;
+        auto value = list.back().value;
+        mp.erase(key);
+        base.set(key, value);
+        list.pop_back();
+      }
+      list.push_front(Node(key, value));
+      mp[key] = list.begin();
+    } else {
+      list.erase(it->second);
+      list.push_front(Node(key, value));
+      mp[key] = list.begin();
+    }
+  }
+  T get(size_t key) {
+    auto it = mp.find(key);
+    if (it == mp.end()) {
+      auto value = base.get(key);
+      if (list.size() == cap) {
+        base.set(list.back().key, list.back().value);
+        mp.erase(list.back().key);
+        list.pop_back();
+      }
+      list.push_front(Node(key, value));
+      mp[key] = list.begin();
+      return value;
+    }
+    {
+      auto value = (*it->second).value;
+      list.erase(it->second);
+      list.push_front(Node(key, value));
+      mp[key] = list.begin();
+      return value;
+    }
+  }
+};
+
+template <class T, int N> class LruMap : public Map<T> {
+  Lru<T, ArrayMap<T>> cache = Lru<T, ArrayMap<T>>(N);
+
+public:
+  T get(size_t key) { return cache.get(key); }
+  void set(size_t key, T value) { cache.set(key, value); }
+};
+
+}; // namespace LevelTSDB
--- test/TestStorage.cpp
+++ test/TestStorage.cpp
@@ -7,90 +7,100 @@
 #include <random>
 #include <sys/resource.h>
 
-typedef LevelTSDB::Storage<uint64_t, LevelTSDB::ArrayMap> TestStorage;
+template <class TestStorage> class Test {
+public:
+  static void testCorrect(uint32_t numCases, int maxn, TestStorage &storage) {
+    std::random_device rd;  // obtain a random number from hardware
+    std::mt19937 gen(rd()); // seed the generator
+    std::uniform_int_distribution<uint64_t> distr(1, maxn); // define the range
 
-static void testCorrect(uint32_t numCases, int maxn, TestStorage &storage) {
-  std::random_device rd;  // obtain a random number from hardware
-  std::mt19937 gen(rd()); // seed the generator
-  std::uniform_int_distribution<uint64_t> distr(1, maxn); // define the range
+    for (uint32_t i = 0; i < numCases; i++) {
+      auto [l, r] = std::tuple{distr(gen), distr(gen)};
+      if (l > r)
+        std::swap(l, r);
+      else if (l == r)
+        continue;
+      if (storage.query(l, r) != (r - l) * (l + r - 1) / 2) {
+        std::cerr << "Failed :" << l << " " << r << std::endl;
+        exit(-1);
+      }
+    }
+    std::cout << "Correction test passed." << std::endl;
+  }
 
-  for (uint32_t i = 0; i < numCases; i++) {
-    auto [l, r] = std::tuple{distr(gen), distr(gen)};
-    if (l > r)
-      std::swap(l, r);
-    else if (l == r)
-      continue;
-    if (storage.query(l, r) != (r - l) * (l + r - 1) / 2) {
-      std::cerr << "Failed :" << l << " " << r << std::endl;
-      exit(-1);
+  static void benchQuery(uint32_t numCases, int maxn, TestStorage &storage) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<uint64_t> distr(1, maxn);
+
+    std::vector<std::tuple<uint32_t, uint32_t>> testCases;
+
+    for (uint32_t i = 1; i <= numCases; i++) {
+      auto [l, r] = std::tuple{distr(gen), distr(gen)};
+      if (l > r) {
+        std::swap(l, r);
+      } else if (l == r) {
+        i--;
+        continue;
+      }
+      testCases.push_back({l, r});
+    }
+    auto start = std::chrono::high_resolution_clock::now();
+    for (auto [l, r] : testCases) {
+      storage.query(l, r);
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = end - start;
+    std::cout << "Average time per query (ns): "
+              << static_cast<double>(
+                     std::chrono::duration_cast<std::chrono::nanoseconds>(
+                         duration)
+                         .count()) /
+                     numCases
+              << std::endl;
+  }
+
+  static TestStorage benchInsertion(int maxn) {
+    TestStorage storage;
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 1; i < maxn; i++) {
+      storage.insert(i);
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = end - start;
+    std::cout << "Average time per insertion (ns): "
+              << static_cast<double>(
+                     std::chrono::duration_cast<std::chrono::nanoseconds>(
+                         duration)
+                         .count()) /
+                     maxn
+              << std::endl;
+    return storage;
+  }
+
+  static void batchTest(uint32_t scale) {
+    for (uint32_t maxn = 100, log10 = 2; log10 < scale; log10++, maxn *= 10) {
+      std::cout << "Dataset size: " << maxn << std::endl;
+      auto storage = benchInsertion(maxn);
+      testCorrect(10000, maxn, storage);
+      benchQuery(10000, maxn, storage);
+      printMemoryUsage();
+      std::cout << std::endl;
     }
   }
-  std::cout << "Correction test passed." << std::endl;
-}
-
-static void benchQuery(uint32_t numCases, int maxn, TestStorage &storage) {
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::uniform_int_distribution<uint64_t> distr(1, maxn);
-
-  std::vector<std::tuple<uint32_t, uint32_t>> testCases;
-
-  for (uint32_t i = 1; i <= numCases; i++) {
-    auto [l, r] = std::tuple{distr(gen), distr(gen)};
-    if (l > r) {
-      std::swap(l, r);
-    } else if (l == r) {
-      i--;
-      continue;
-    }
-    testCases.push_back({l, r});
-  }
-  auto start = std::chrono::high_resolution_clock::now();
-  for (auto [l, r] : testCases) {
-    storage.query(l, r);
-  }
-
-  auto end = std::chrono::high_resolution_clock::now();
-  auto duration = end - start;
-  std::cout << "Average time per query (ns): "
-            << static_cast<double>(
-                   std::chrono::duration_cast<std::chrono::nanoseconds>(
-                       duration)
-                       .count()) /
-                   numCases
-            << std::endl;
-}
-
-static TestStorage benchInsertion(int maxn) {
-  TestStorage storage;
-  auto start = std::chrono::high_resolution_clock::now();
-  for (int i = 1; i < maxn; i++) {
-    storage.insert(i);
-  }
-  auto end = std::chrono::high_resolution_clock::now();
-  auto duration = end - start;
-  std::cout << "Average time per insertion (ns): "
-            << static_cast<double>(
-                   std::chrono::duration_cast<std::chrono::nanoseconds>(
-                       duration)
-                       .count()) /
-                   maxn
-            << std::endl;
-  return storage;
-}
-
-static void batchTest() {
-  for (uint32_t maxn = 100, log10 = 2; log10 < 9; log10++, maxn *= 10) {
-    std::cout << "Dataset size: " << maxn << std::endl;
-    auto storage = benchInsertion(maxn);
-    testCorrect(10000, maxn, storage);
-    benchQuery(10000, maxn, storage);
-    printMemoryUsage();
-    std::cout << std::endl;
-  }
-}
+};
 
 int main() {
-  batchTest();
+  using LevelTSDB::ArrayMap;
+  using LevelTSDB::LruMap;
+  using LevelTSDB::Map;
+  using LevelTSDB::Storage;
+  std::cout << "Testing: ArrayMap<uint64_t>"
+            << "\n";
+  Test<Storage<uint64_t, ArrayMap<uint64_t>>>::batchTest(9);
+  std::cout << "Testing: LruMap<uint64_t, 10000>>"
+            << "\n";
+  Test<Storage<uint64_t, LruMap<uint64_t, 10000>>>::batchTest(8);
   return 0;
 }
