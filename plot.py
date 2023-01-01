6326bbe3326c2c86d4fef076e8cbaa0a3928b100
YingChi Long <me@inclyc.cn>
2023-01-01 11:51:34 +0800

    [ci] automatically upload result images

--- plot.py
+++ plot.py
@@ -2,20 +2,28 @@ from sys import stdin
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
-
+import argparse
 
 def main():
-    data = pd.read_csv("result.txt")
-    fig, ax = plt.subplots(3, 1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output', default='test-storage.pdf')
+    args = parser.parse_args()
+
+    data = pd.read_csv(stdin)
+    data['Name'] = data['Name'].map(lambda x:
+        x.removeprefix('SINGLE_ARG(Storage<uint64_t, ')
+         .removesuffix('>)'))
+    print(data)
+    fig, ax = plt.subplots(3, 1, figsize=(9, 15))
     sns.lineplot(data=data, x='Dataset Size',
                  y="Insertion (ns)", hue="Name", ax=ax[0])
     sns.lineplot(data=data, x='Dataset Size',
                  y="Query (ns)", hue="Name", ax=ax[1])
     sns.lineplot(data=data, x='Dataset Size',
-                 y="External Invokes", hue="Name", ax=ax[2])
+                 y="Average External Accesses", hue="Name", ax=ax[2])
     for a in ax:
         a.set_xscale('log')
-    plt.show()
+    plt.savefig(args.output)
 
 
 if __name__ == '__main__':
