fb4d770192b6a43ae88fbbcbc3a13ad1d2138c25
Xanbert <96752698+Xanbert@users.noreply.github.com>
2023-03-01 23:02:47 +0800

    test: add distribution test (#12)

--- test/TestStorage.cpp
+++ test/TestStorage.cpp
@@ -1,6 +1,7 @@
 #include "Data.h"
 #include <cassert>
 #include <chrono>
+#include <cmath>
 #include <cstdint>
 #include <iostream>
 #include <random>
@@ -51,6 +52,51 @@ public:
   [[gnu::pure]] bool shouldValidate() override { return false; }
 
   virtual ~RandomDistribution() = default;
+};
+template <class T, int A, int O>
+class SineDistribution : public ElementDistribution<T> {
+public:
+  [[nodiscard]] T get(std::size_t x) override {
+    return static_cast<T>(A * sin(x) + O);
+  }
+  [[noreturn]] T sum([[maybe_unused]] std::size_t l,
+                     [[maybe_unused]] std::size_t r) override {
+    __builtin_unreachable();
+  }
+  [[gnu::pure]] bool shouldValidate() override { return false; }
+
+  virtual ~SineDistribution() = default;
+};
+
+template <class T, int S, int M>
+class GaussianDistribution : public ElementDistribution<T> {
+public:
+  [[nodiscard]] T get(std::size_t x) override {
+    return static_cast<T>((1 / (S * sqrt(2 * M_PI))) *
+                          exp(-0.5 * pow((x - M) / S, 2.0)));
+  }
+  [[noreturn]] T sum([[maybe_unused]] std::size_t l,
+                     [[maybe_unused]] std::size_t r) override {
+    __builtin_unreachable();
+  }
+  [[gnu::pure]] bool shouldValidate() override { return false; }
+
+  virtual ~GaussianDistribution() = default;
+};
+
+template <class T, int L, int O, int A>
+class ExpDistribution : public ElementDistribution<T> {
+public:
+  [[nodiscard]] T get(std::size_t x) override {
+    return static_cast<T>(O + A * exp(-L * x));
+  }
+  [[noreturn]] T sum([[maybe_unused]] std::size_t l,
+                     [[maybe_unused]] std::size_t r) override {
+    __builtin_unreachable();
+  }
+  [[gnu::pure]] bool shouldValidate() override { return false; }
+
+  virtual ~ExpDistribution() = default;
 };
 
 [[nodiscard]] auto
@@ -125,16 +171,20 @@ public:
     auto nanoDuration = duration_cast<nanoseconds>(end - start).count();
     return {std::move(storage), static_cast<double>(nanoDuration) / maxn};
   }
-
-  static void batchTest(uint32_t scale, const char *name,
-                        ElementDistribution<typename S::DataTy> &ED) {
+  template <class ED_TYPE>
+  static void batchTest(uint32_t scale, const char *name, const char *ed_type) {
+    ED_TYPE ED;
     for (uint32_t maxn = 100, log10 = 2; log10 < scale; log10++, maxn *= 10) {
       auto [storage, insertionTime] = benchInsertion(maxn, ED);
       if (ED.shouldValidate())
         validate(10000, maxn, storage, ED);
       auto [queryTime, externalInvokes] = benchQuery(10000, maxn, storage);
       std::cout << insertionTime << "," << queryTime << "," << externalInvokes
-                << "," << maxn << "," << name << "\n";
+                << "," << maxn << ","
+                << "\"" << name << "\""
+                << ","
+                << "\"" << ed_type << "\""
+                << "\n";
     }
   }
 };
@@ -147,33 +197,74 @@ int main() {
   using LevelTSDB::Map;
   using LevelTSDB::Storage;
 
-  std::cout
-      << "Insertion (ns), Query (ns), External Invokes, Dataset Size, Name"
-      << "\n";
+  std::cout << "Insertion (ns),Query (ns),External Invokes,Dataset "
+               "Size,Name,Distribution Type"
+            << "\n";
 
-#define TEST_STORAGE(TYPE, BATCH, ED) Test<TYPE>::batchTest(BATCH, #TYPE, ED)
+#define TEST_STORAGE(TYPE, BATCH, ED_TYPE)                                     \
+  Test<TYPE>::batchTest<ED_TYPE>(BATCH, #TYPE, #ED_TYPE)
 #define SINGLE_ARG(...) __VA_ARGS__
 
-  auto ED = NaturalNumbersDistribution<uint64_t>();
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, ArrayMap<uint64_t, 100>>), 8,
+               SINGLE_ARG(NaturalNumbersDistribution<uint64_t>));
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, ArrayMap<uint64_t, 1000>>), 8,
+               SINGLE_ARG(NaturalNumbersDistribution<uint64_t>));
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, ArrayMap<uint64_t, 10000>>), 8,
+               SINGLE_ARG(NaturalNumbersDistribution<uint64_t>));
 
-  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, ArrayMap<uint64_t, 100>>), 8, ED);
-  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, ArrayMap<uint64_t, 1000>>), 8, ED);
-  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, ArrayMap<uint64_t, 10000>>), 8, ED);
-
-  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 1000>>), 8, ED);
-  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 10000>>), 8, ED);
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 1000>>), 8,
+               SINGLE_ARG(NaturalNumbersDistribution<uint64_t>));
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 10000>>), 8,
+               SINGLE_ARG(NaturalNumbersDistribution<uint64_t>));
 
   // Test for different disturbutions
 
-  auto ED2 = RandomDistribution<uint64_t>();
-
-  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, ArrayMap<uint64_t, 100>>), 8, ED2);
-  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, ArrayMap<uint64_t, 1000>>), 8, ED2);
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, ArrayMap<uint64_t, 100>>), 8,
+               SINGLE_ARG(RandomDistribution<uint64_t>));
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, ArrayMap<uint64_t, 1000>>), 8,
+               SINGLE_ARG(RandomDistribution<uint64_t>));
   TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, ArrayMap<uint64_t, 10000>>), 8,
-               ED2);
+               SINGLE_ARG(RandomDistribution<uint64_t>));
 
-  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 1000>>), 8, ED2);
-  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 10000>>), 8, ED2);
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 1000>>), 8,
+               SINGLE_ARG(RandomDistribution<uint64_t>));
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 10000>>), 8,
+               SINGLE_ARG(RandomDistribution<uint64_t>));
 
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, ArrayMap<uint64_t, 100>>), 8,
+               SINGLE_ARG(SineDistribution<uint64_t, 100, 10000>));
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, ArrayMap<uint64_t, 1000>>), 8,
+               SINGLE_ARG(SineDistribution<uint64_t, 100, 10000>));
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, ArrayMap<uint64_t, 10000>>), 8,
+               SINGLE_ARG(SineDistribution<uint64_t, 100, 10000>));
+
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 1000>>), 8,
+               SINGLE_ARG(SineDistribution<uint64_t, 100, 10000>));
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 10000>>), 8,
+               SINGLE_ARG(SineDistribution<uint64_t, 100, 10000>));
+
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, ArrayMap<uint64_t, 100>>), 8,
+               SINGLE_ARG(GaussianDistribution<uint64_t, 100, 10000>));
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, ArrayMap<uint64_t, 1000>>), 8,
+               SINGLE_ARG(GaussianDistribution<uint64_t, 100, 10000>));
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, ArrayMap<uint64_t, 10000>>), 8,
+               SINGLE_ARG(GaussianDistribution<uint64_t, 100, 10000>));
+
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 1000>>), 8,
+               SINGLE_ARG(GaussianDistribution<uint64_t, 100, 10000>));
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 10000>>), 8,
+               SINGLE_ARG(GaussianDistribution<uint64_t, 100, 10000>));
+
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, ArrayMap<uint64_t, 100>>), 8,
+               SINGLE_ARG(ExpDistribution<uint64_t, 4, 1000, 1000>));
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, ArrayMap<uint64_t, 1000>>), 8,
+               SINGLE_ARG(ExpDistribution<uint64_t, 4, 1000, 1000>));
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, ArrayMap<uint64_t, 10000>>), 8,
+               SINGLE_ARG(ExpDistribution<uint64_t, 4, 1000, 1000>));
+
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 1000>>), 8,
+               SINGLE_ARG(ExpDistribution<uint64_t, 4, 1000, 1000>));
+  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 10000>>), 8,
+               SINGLE_ARG(ExpDistribution<uint64_t, 4, 1000, 1000>));
   return 0;
 }
