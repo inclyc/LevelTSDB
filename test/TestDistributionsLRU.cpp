bc8cca8c5a34e8d48e50c328872f79b67617efb6
Yingchi Long <i@lyc.dev>
2023-04-03 15:04:28 +0800

    test/lru: multithreaded LRU test

--- test/TestDistributionsLRU.cpp
+++ test/TestDistributionsLRU.cpp
@@ -1,27 +1,44 @@
 #include "Data.h"
 #include "Map.h"
 #include "Test.h"
+#include <thread>
+#include <vector>
 
 int main() {
   using namespace LevelTSDB; // Test for different disturbutions
 
-  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 1000>>), 8,
-               SINGLE_ARG(RandomDistribution<uint64_t>));
-  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 10000>>), 8,
-               SINGLE_ARG(RandomDistribution<uint64_t>));
+  std::vector<std::thread> threads;
 
-  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 1000>>), 8,
-               SINGLE_ARG(SineDistribution<uint64_t, 100, 10000>));
-  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 10000>>), 8,
-               SINGLE_ARG(SineDistribution<uint64_t, 100, 10000>));
+  threads.push_back(std::thread([]() {
+    TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 1000>>), 8,
+                 SINGLE_ARG(RandomDistribution<uint64_t>));
+    TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 10000>>), 8,
+                 SINGLE_ARG(RandomDistribution<uint64_t>));
+  }));
 
-  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 1000>>), 8,
-               SINGLE_ARG(GaussianDistribution<uint64_t, 100, 10000>));
-  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 10000>>), 8,
-               SINGLE_ARG(GaussianDistribution<uint64_t, 100, 10000>));
+  threads.push_back(std::thread([]() {
+    TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 1000>>), 8,
+                 SINGLE_ARG(SineDistribution<uint64_t, 100, 10000>));
+    TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 10000>>), 8,
+                 SINGLE_ARG(SineDistribution<uint64_t, 100, 10000>));
+  }));
 
-  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 1000>>), 8,
-               SINGLE_ARG(ExpDistribution<uint64_t, 4, 1000, 1000>));
-  TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 10000>>), 8,
-               SINGLE_ARG(ExpDistribution<uint64_t, 4, 1000, 1000>));
+  threads.push_back(std::thread([]() {
+    TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 1000>>), 8,
+                 SINGLE_ARG(GaussianDistribution<uint64_t, 100, 10000>));
+    TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 10000>>), 8,
+                 SINGLE_ARG(GaussianDistribution<uint64_t, 100, 10000>));
+  }));
+
+  threads.push_back(std::thread([]() {
+    TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 1000>>), 8,
+                 SINGLE_ARG(ExpDistribution<uint64_t, 4, 1000, 1000>));
+    TEST_STORAGE(SINGLE_ARG(Storage<uint64_t, LruMap<uint64_t, 10000>>), 8,
+                 SINGLE_ARG(ExpDistribution<uint64_t, 4, 1000, 1000>));
+  }));
+
+  for (auto &t : threads) {
+    t.join();
+  }
+  return 0;
 }
